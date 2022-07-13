358446d149dd3cd33e115577bea14264041f2d64
Miikor-Jnr <barilelomiikor@gmail.com>
2022-07-13 20:21:58 +0100

    corrections effected on the signup and login page

--- src/components/FormElemets/Select/index.js
+++ src/components/FormElemets/Select/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-export function Select() {
+export function Select({img}) {
   return (
-    <div className="input-field">
+  <div className="input-field">
       <div className="input-icon-wrapper">
-        <img src="../images/Mail.png" className="input-ico"></img>
+        <img src={img} className="input-ico"></img>
       </div>
-      <input type="email" className="signup-input" placeholder="E-mail"></input>
-    </div>
+      <select className="signup-input">
+        <option className="option-one" disabled>Select account type</option>
+      </select>
+</div>
   );
 }
+
+
