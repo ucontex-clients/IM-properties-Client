692898649d2371fdebd6369a94eecc6e7eb2dc86
Miikor-Jnr <barilelomiikor@gmail.com>
2022-08-03 00:21:25 +0100

    Started the dashboard referral page

--- src/components/DashboardComponents/DashboardReferral/index.js
+++ src/components/DashboardComponents/DashboardReferral/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { DashboardElement } from "../../"
+import DashboardReferralHistory from "../DashboardReferralHistory"
 
 export function DashboardReferral(){
     return(
@@ -8,11 +10,33 @@ export function DashboardReferral(){
                 <p>Referral</p>
             </div>
 
-            <div>
-                <h1>
-                    Check
-                </h1>
+            <div className="referral-main-container">
+                <DashboardElement logoBackground='rgba(61, 178, 255, 0.2)'
+                logoURL='./images/houseLogo.png' number='0' text='Total Referral'/>
+                <DashboardElement logoBackground='rgba(255, 152, 0, 0.2)'
+                logoURL='./images/accountBalance.png' number='0' text='Total Earning'/>
+                <div className="referral-link-container">
+                    <p>Referral Link</p>
+                    <p className="referral-link">https://improperties.com/register?u=IM1234509</p>
+                    <div className="copy-image-wrapper">
+                        <div><img src="./images/Copy.png"></img></div>
+                        <p>Copy</p>
+                    </div>
+                </div>
+                <div className="referral-commission-wrapper">
+                    <div className="item4">
+                        <p>Level 1 Commission</p>
+                        <p>Refer someone to purchase any property and get 10% commision </p>
+                    </div>
+                    <div className="item5">
+                        <p>Level 2 Commission</p>
+                        <p>The person you referred, gets someone to purchase a property you get 
+                            2% of the  level 1 commission</p>
+                    </div>
+                </div>
             </div>
+
+            <DashboardReferralHistory />
         </div>
     )
 }
