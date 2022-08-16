835ac0bc3b179a9081b139585f7db596429bbd20
Miikor-Jnr <barilelomiikor@gmail.com>
2022-08-16 23:42:29 +0100

    Made changes to the calendar

--- src/components/PaymentComponents/OneOffPayment/index.js
+++ src/components/PaymentComponents/OneOffPayment/index.js
@@ -10,11 +10,14 @@ export function OneOffPayment({changePaymentMode}){
             <p className="payment-amount-para">N1,840,000</p>
             <div className="payment-dropdown-div">
                 <label htmlFor='payment-plan' style={{marginBottom: '26px', color: '#6C6C6C'}}>Payment plan</label>
+                <div style={{position:'relative'}}>
                 <select value='one-off' onChange={(e) => changePaymentMode(e)}>
                     <option className="option" value='one-off'>Out right payment</option>
                     <option className="option" value='installment'>Installmental payment</option>
                     <option className="option" value='easy-buy'>Easy-buy plan</option>
                 </select>
+                <div className="fill-drop-wrapper" style={{right:'13.11px'}}><img src="./images/arrowDown.png"></img></div>
+                </div>
             </div>
 
             <div className="payment-dropdown-div" style={{marginTop: '43px'}}>
@@ -24,11 +27,14 @@ export function OneOffPayment({changePaymentMode}){
 
             <div className="payment-dropdown-div" style={{marginTop: '47px'}}>
                 <label htmlFor='payment-plan' style={{marginBottom: '31px', fontWeight:'700'}}>Payment Method</label>
-                <select name="payment" value='0'>
-                    <option className="option">Paystack (card, USSD, Bank Transfer)</option>
-                    <option className="option">My Wallet</option>
-                    <option className="option">Bank Deposit</option>
-                </select>
+                <div style={{position:'relative'}}>
+                    <select name="payment" value='0'>
+                        <option className="option">Paystack (card, USSD, Bank Transfer)</option>
+                        <option className="option">My Wallet</option>
+                        <option className="option">Bank Deposit</option>
+                    </select>
+                <div className="fill-drop-wrapper" style={{right:'13.11px'}}><img src="./images/arrowDown.png"></img></div>
+                </div>
             </div>
 
             <div className="pay-now-button-holder">
