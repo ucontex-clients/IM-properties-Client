e2eeea9b86d4c833a7cb3e318fb99f2d97100665
Miikor-Jnr <barilelomiikor@gmail.com>
2022-10-18 23:15:50 +0100

    Update index.js

--- src/components/Nav1/index.js
+++ src/components/Nav1/index.js
@@ -1,11 +1,30 @@
-import React from "react"
-import {BiUserCircle} from "react-icons/bi"
+import React from "react";
+import { BiUserCircle } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-export function Nav1({signIn, signUp}){
-    return(
-        <div className="navigation-buttons-wrapper">
-            <BiUserCircle style={{fontSize:'30px'}} />
-            <p style={{fontFamily:'Montserrat', fontWeight:'500', fontSize:'15px'}}>Account</p>
-        </div>
-    )
+export function Nav1({ signIn, signUp }) {
+  return (
+    <div className="navigation-buttons-wrapper">
+      <div className="account-modal">
+        <ul className="p-[15px]">
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+      <BiUserCircle style={{ fontSize: "30px" }} />
+      <p
+        style={{
+          fontFamily: "Montserrat",
+          fontWeight: "500",
+          fontSize: "15px",
+        }}
+      >
+        Account
+      </p>
+    </div>
+  );
 }
