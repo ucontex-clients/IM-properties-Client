540e17fd8a1a957cce2cf54e684ec5941d285a32
Miikor-Jnr <barilelomiikor@gmail.com>
2022-11-09 19:42:44 +0100

    Corrected the confirm page

--- src/components/DashboardComponents/DashboardPayment/index.js
+++ src/components/DashboardComponents/DashboardPayment/index.js
@@ -1,34 +1,42 @@
-import React from "react"
+import React from "react";
 
-export function DashboardPayment(){
-    return (
-        <div className="dashboard-main-right bg-white">
-        <ul className="payment-grids">
-            <li>Trans. ID</li>
-            <li>Property</li>
-            <li>Amount</li>
-            <li>Mode</li>
-            <li>Paid</li>
-            <li>Balance</li>
-            <li>Duration</li>
-            <li>Next Payment</li>
-            <li>Action</li>
-        </ul>
-        <ul className="payment-grids">
-            <li>IM 0023</li>
-            <li>Campus Gardens Estate</li>
-            <li>N1,865,000</li>
-            <li>Outright</li>
-            <li>1,865,000</li>
-            <li>Completed</li>
-            <li>None</li>
-            <li>Nil</li>
-            <li className="flex items-center">
-                <p>History</p>
-                <button>Pay Now</button>
-            </li>
-        </ul>
-        </div>
-
-    )
+export function DashboardPayment() {
+  return (
+    <div className="dashboard-main-right bg-white">
+      <ul className="payment-grids">
+        <li>Trans. ID</li>
+        <li>Property</li>
+        <li>Amount</li>
+        <li>Mode</li>
+        <li>Paid</li>
+        <li>Balance</li>
+        <li>Duration</li>
+        <li>Next Payment</li>
+        <li>Action</li>
+      </ul>
+      <ul className="payment-grids">
+        <li>IM 0023</li>
+        <li>Campus Gardens Estate</li>
+        <li>N1,865,000</li>
+        <li>Outright</li>
+        <li>1,865,000</li>
+        <li>Completed</li>
+        <li>None</li>
+        <li>Nil</li>
+        <li className="flex gap-x-[10px] items-center">
+          <p>History</p>
+          <button
+            className="px-[13px] py-[4px] font-fam font-bold text-[12px]"
+            style={{
+              background: "rgba(61, 178, 255, 0.25)",
+              borderRadius: "20px",
+              color: "#0682D3",
+            }}
+          >
+            Pay Now
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
