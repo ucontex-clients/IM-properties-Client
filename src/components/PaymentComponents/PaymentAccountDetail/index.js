9e4ef85a1002b5acfe2aba83473c873750129b10
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2022-12-29 15:04:26 +0100

    Update index.js

--- src/components/PaymentComponents/PaymentAccountDetail/index.js
+++ src/components/PaymentComponents/PaymentAccountDetail/index.js
@@ -27,7 +27,7 @@ export function PaymentAccountDetail(props) {
       description: 'Payment for items in cart for IM-Properties',
       logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
     },
-    redirect_url: "http://localhost:3000/dashboard"
+    redirect_url: "http://localhost:3000/dashboard/home"
   };
 
   const handleFlutterPayment = useFlutterwave(config);
